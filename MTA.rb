4f8b90dfdda7496f2c85971babbd58e5dd345308
Gary Siu <Gary@untypeable.com>
2015-08-27 20:06:13 +0100

    Finish getting validated input. On to the math

--- MTA.rb
+++ MTA.rb
@@ -1,15 +1,37 @@
 lines = {
-  'n' =>  %w[gc 33 28 23 us ap],
+  'n' =>  %w[ts 34 28 23 us 8],
   'l' => %w[8 6 us 3 1],
   '6' => %w[gc 33 28 23 us ap]
 }
 
-puts 'What is your starting line? (n, l, 6)'
+puts 'What is your starting line? (' + lines.keys.join(', ') +  ')'
 start_line = gets.chomp.downcase
 
-while start_line != 'n' && start_line != 'l' && start_line != '6'
-    puts "What is your starting line? (n, l, 6)"
+while ! lines.include? start_line
+    puts 'What is your starting line? (' + lines.keys.join(', ') +  ')'
     start_line = gets.chomp.downcase
 end
 
-puts 'Which station on that line? (' + lines[start_line].join(', ') + ')' 
+puts 'Which station on that line? (' + lines[start_line].join(', ') + ')'
+start_station = gets.chomp.downcase
+
+while ! lines[start_line].include? start_station
+  puts 'Which station on that line? (' + lines[start_line].join(', ') + ')'
+  start_station = gets.chomp.downcase
+end
+
+puts 'What is your destination line? (' + lines.keys.join(', ') +  ')'
+end_line = gets.chomp.downcase
+
+while ! lines.include? end_line
+    puts 'What is your destination line? (' + lines.keys.join(', ') +  ')'
+    end_line = gets.chomp.downcase
+end
+
+puts 'Which station on that line? (' + lines[end_line].join(', ') + ')'
+end_station = gets.chomp.downcase
+
+while ! lines[start_line].include? start_station
+  puts 'Which station on that line? (' + lines[end_line].join(', ') + ')'
+  end_station = gets.chomp.downcase
+end
