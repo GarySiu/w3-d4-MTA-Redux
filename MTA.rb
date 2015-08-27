9f7ae97a6e8976c0cbe807051d0cdb86aa75efba
Gary Siu <Gary@untypeable.com>
2015-08-27 22:44:04 +0100

    Refactored with string interpolation. I love it.

--- MTA.rb
+++ MTA.rb
@@ -12,39 +12,39 @@ end_station = 'undeclared'
 
 while again != 'n'
   while ! lines.include? start_line
-      puts 'What is your starting line? (' + lines.keys.join(', ') +  ')'
+      puts "What is your starting line? (#{lines.keys.join(', ')})"
       start_line = gets.chomp.downcase
   end
 
   while ! lines[start_line].include? start_station
-    puts 'Which station on that line? (' + lines[start_line].join(', ') + ')'
+    puts "Which station on that line? (#{lines[start_line].join(', ')})"
     start_station = gets.chomp.downcase
   end
 
   while ! lines.include? end_line
-      puts 'What is your destination line? (' + lines.keys.join(', ') +  ')'
+      puts "What is your destination line? (#{lines.keys.join(', ')})"
       end_line = gets.chomp.downcase
   end
 
   while ! lines[end_line].include? end_station
-    puts 'Which station on that line? (' + lines[end_line].join(', ') + ')'
+    puts "Which station on that line? (#{lines[end_line].join(', ')})"
     end_station = gets.chomp.downcase
   end
 
   if start_line == end_line
     stops = lines[start_line].index(start_station) - lines[end_line].index(end_station)
     stops = stops.abs()
-    puts 'Your journey is ' + stops.to_s + ' stops.'
+    puts "Your journey is #{stops.to_s} stops."
   else
     step1 = lines[start_line].index(start_station) - lines[start_line].index('us')
     step2 = lines[end_line].index(end_station) - lines[end_line].index('us')
     stops = step1.abs() + step2.abs()
-    puts 'Your journey is ' + stops.to_s + ' stops.'
+    puts "Your journey is #{stops.to_s} stops."
   end
   
   while again != 'y' && again != 'n'
     puts 'Do you want to calculate another journey? (y/n)'
     again = gets.chomp.downcase
   end
-  
+
 end
