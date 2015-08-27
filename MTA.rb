0438f31bdd544498bc8b44c2f5610b6320c22164
Gary Siu <Gary@untypeable.com>
2015-08-27 19:28:25 +0100

    Added an array of stops

--- MTA.rb
+++ MTA.rb
@@ -1,3 +1,9 @@
+lines = {
+  'n' =>  %w[gc 33 28 23 us ap],
+  'l' => %w[8 6 us 3 1],
+  '6' => %w[gc 33 28 23 us ap]
+}
+
 puts "What is your starting line? (n, l, 6)"
 start_line = gets.chomp.downcase
 while start_line != 'n' && start_line != 'l' && start_line != '6'
